c77e4e2dc26f5d0a8764482e2819d7824bf306d8
Karl <karl@fwdmotion.studio>
2019-08-16 08:31:33 -0500

    Updated some CSS

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -7,9 +7,9 @@
 @section('content')
     <div class="flex-container">
         @foreach($articles as $article)
-            <div class="flex-item">
+            <div class="flex-item blog-item">
                 @isset($article->featured_image)
-                    <img src={{ env('APP_STORAGE_URL').$article->featured_image}}>
+                    <img src={{ env('APP_STORAGE_URL').$article->featured_image }}>
                 @endisset
                 <a href="/blog/view/{{$article->id}}"><h3>{{$article->title}}</h3></a>
                 @isset($article->category)
