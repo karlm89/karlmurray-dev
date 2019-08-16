7b7cdfb7279356a748aca978225a833231f1759f
Karl <karl@fwdmotion.studio>
2019-08-16 08:45:09 -0500

    limiting blog images to 320

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -5,18 +5,20 @@
 @endsection
 
 @section('content')
-    <div class="flex-container">
-        @foreach($articles as $article)
-            <div class="flex-item blog-item">
-                @isset($article->featured_image)
-                    <img src={{ env('APP_STORAGE_URL').$article->featured_image }}>
-                @endisset
-                <a href="/blog/view/{{$article->id}}"><h3>{{$article->title}}</h3></a>
-                @isset($article->category)
-                    <p>Category : {{$article->category}}</p>
-                @endisset
-                <p>{{$article->created_at}}</p>
-            </div>
-        @endforeach
-    </div>
+    @if(count($articles)>0)
+        <div class="flex-container">
+            @foreach($articles as $article)
+                <div class="flex-item blog-item">
+                    @isset($article->featured_image)
+                        <img src={{ env('APP_STORAGE_URL').$article->featured_image }}>
+                    @endisset
+                    <a href="/blog/view/{{$article->id}}"><h3>{{$article->title}}</h3></a>
+                    @isset($article->category)
+                        <p>Category : {{$article->category}}</p>
+                    @endisset
+                    <p>{{$article->created_at}}</p>
+                </div>
+            @endforeach
+        </div>
+    @endif
 @endsection
