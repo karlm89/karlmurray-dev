fc75695e7ff0597038a41b304f84ec30dd764537
Karl M <karl@fwdmotion.studio>
2019-08-19 18:48:41 -0500

    Finishing filling up the index

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $projects = DB::table('projects')->get();
+        $articles = DB::table('articles')->get();
+        $testimonials = DB::table('testimonials')->get();
+
+        return view('index',compact('articles','projects','testimonials'));
     }
 
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,5 +5,30 @@
 @endsection
 
 @section('content')
-    <center><h1>Karl Murray</h1></center>
+    <h1>Karl Murray</h1>
+    <div class="flex-container">
+        <div class="flex-item">
+            <h4>Projects</h4>
+            @foreach($projects as $project)
+                <a href="/project/{{$project->slug}}">{{ $project->title}}</a>
+            @endforeach
+        </div>
+        <div class="flex-item">
+            <h4>Blog Posts</h4>
+            @foreach($articles as $article)
+                <a href="/blog/{{$article->slug}}">" {{ $article->title}} "</a>
+            @endforeach
+        </div>
+    </div>
+    @isset($testimonials)
+        <div class="fullwidth-container">
+            <h4>Testimonials</h4>
+            @foreach($testimonials as $data)
+                <p>
+                    "{{ $data->text}}"
+                    - {{ $data->author }}
+                </p>
+            @endforeach
+        </div>
+    @endisset
 @endsection
