ed2da1719e72d294bd73243952342a5425f4b1f9
Karl M <karl@fwdmotion.studio>
2019-08-19 18:14:19 -0500

    Created project pages

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -11,8 +11,9 @@
             @foreach($projects as $project)
                 <div class="flex-item">
                     <img src={{ env('APP_STORAGE_URL').$project->image }}>
-                    <h3>{{ $project->title }}</h3>
+                    <a href="/project/{{ $project->slug}}"><h3>{{ $project->title }}</h3></a>
                     <a class="link" href={{ $project->link }} target="_blank">{{ $project->link }}</a>
+                    <p>Stack : {{ $project->stack }}</p>
                 </div>
             @endforeach 
         </div>
