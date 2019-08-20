45016b5523eb90a24ad51591dbee07b72641cdb6
Karl M <karl@fwdmotion.studio>
2019-08-19 20:34:45 -0500

    OrderBy project title

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -9,7 +9,7 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = DB::table('projects')->get();
+        $projects = DB::table('projects')->orderBy('title')->get();
 
         return view('projects', compact('projects'));
     }
