62f2620d6bd6f7d7a4916b1fce7dd4d5c1d00f39
Karl Murray <karl@fwdmotion.studio>
2020-08-24 11:50:48 -0500

    Fixed the domain change.

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,5 +1,5 @@
 <head>
-    <title>@yield('title') | karlmurray.dev</title>
+    <title>@yield('title') | karlm.dev</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="#" type="image/x-icon" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
@@ -10,7 +10,7 @@
 <body>
     <div class="container-fluid">
         <nav class="navbar fixed-top navbar-expand-lg navbar-light">
-        <a class="navbar-brand" href="/">karlmurray.dev</a>
+        <a class="navbar-brand" href="/">karlm.dev</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -45,8 +45,7 @@
     <div class="footer-row">
         <div class="footer-row-item">
             <h4>Contact Info :</h4>
-            <a href="tel:8324226129">(979)-645-0658</a><br>
-            <a href="mailto:karl@karlmurray.dev">karl@karlmurray.dev</a> &nbsp;
+            <a href="mailto:karl@karlm.dev">karl@karlm.dev</a> &nbsp;
         </div>
         <div class="footer-row-item">
             <h4>Quick Links :</h4>
@@ -63,7 +62,7 @@
             <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-            <div class="credit">&copy; 2019 karlmurray.dev</div>
+            <div class="credit">&copy; 2019 karlm.dev</div>
         </div>
     </footer>
 </body>
